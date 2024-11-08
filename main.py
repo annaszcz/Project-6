af1334b025239ca39ac9eafec6c4024abffbc8eb
annaszcz <anna.szczepara@gmail.com>
2024-11-08 14:39:01 +0100

    first_commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_data():
-    url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BTC&apikey=B6EHIX42BOYPDA9B."
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BTC&apikey=API_KEY."
     response = requests.get(url)
     values = response.json()
     print(values)
