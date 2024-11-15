90d39fd1ac9e61541f7c7dbf05af19eb40b72c56
annaszcz <anna.szczepara@gmail.com>
2024-11-15 17:53:05 +0100

    aws_lambda_functions

--- aws_railway/save/lambda_saving.py
+++ aws_railway/save/lambda_saving.py
@@ -1,10 +1,27 @@
 import datetime
-import pandas as pd
+# import pandas as pd
+import os
+import psycopg2
+from dotenv import load_dotenv
 
+# Load the .env file
+load_dotenv()
+
+# Database connection parameters
+DATABASE_URL = os.getenv('url')
+print(DATABASE_URL)
+
+# Connect to the database
+conn = psycopg2.connect(DATABASE_URL)
+
+cur = conn.cursor()
+
+print("connected")
 def save_data(event, context):
+    # fetch data from api
     # Parse the Data as JSON
-    # response = event   #this is just to Test the lambda function from the Test Event Json
-    # response = event['responsePayload'] #this to get the returned data from the 1st lambda function 
+    # data = event   #this is just to Test the lambda function from the Test Event Json
+    data= event['responsePayload'] #this to get the returned data from the 1st lambda function 
 
     # Prepare lists for each column
     dates = []
@@ -26,14 +43,14 @@ def save_data(event, context):
         volumes.append(int(data["Time Series (Daily)"][date_str]['5. volume']))
 
     # Convert lists into a DataFrame (optional, for verification or further processing)
-    df = pd.DataFrame({
-        "date": dates,
-        "open": open_prices,
-        "high": high_prices,
-        "low": low_prices,
-        "close": close_prices,
-        "volume": volumes
-    })
+    # df = pd.DataFrame({
+    #    "date": dates,
+    #   "open": open_prices,
+    #    "high": high_prices,
+    #    "low": low_prices,
+    #    "close": close_prices,
+    #    "volume": volumes
+    # })
 
     # Insert each row individually to handle potential unique constraints
     for date, open_price, high_price, low_price, close_price, vol in zip(dates, open_prices, high_prices, low_prices, close_prices, volumes):
